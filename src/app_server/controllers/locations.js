02e6e11736e4ca9264b7510a0f0ff7484cd21ada
Bruno <brunohgrau99@gmail.com>
2024-02-14 00:22:04 -0300

    4.4

--- src/app_server/controllers/locations.js
+++ src/app_server/controllers/locations.js
@@ -7,13 +7,13 @@ const homelist = (req, res) => {
 /* GET 'Location info' page */
 
 const locationInfo = (req, res) => {
-  res.render("index", { title: "Location info" });
+  res.render("location-info", { title: "Location info" });
 };
 
 /* GET 'Add review' page */
 
 const addReview = (req, res) => {
-  res.render("index", { title: "Add review" });
+  res.render("location-review-form", { title: "Add review" });
 };
 
 module.exports = { homelist, locationInfo, addReview };
