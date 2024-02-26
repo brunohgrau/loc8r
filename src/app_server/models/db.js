3393d39fa3ee9a0323be8aeddc3c7242527f8370
Bruno <brunohgrau99@gmail.com>
2024-02-26 00:56:33 -0300

    DB Setup

--- src/app_server/models/db.js
+++ src/app_server/models/db.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const dbURI = "mongodb://localhost/Loc8r";
 const readline = require("readline");
 
+// Readline
+
 if (process.platform === "win32") {
   const rl = readline.createInterface({
     input: process.stdin,
@@ -11,6 +13,8 @@ if (process.platform === "win32") {
     process.emit("SIGINT");
   });
 }
+
+// Mongoose Connect
 mongoose.connect(dbURI, { useNewUrlParser: true });
 
 // Connection Events
@@ -25,4 +29,28 @@ mongoose.connection.on("error", (err) => {
 
 mongoose.connection.on("disconnected", () => {
   console.log("Mongoose disconnected");
+});
+
+// Graceful Shutdown
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close(() => {
+    console.log(`Mongoose disconected through ${msg}`);
+    callback();
+  });
+};
+
+// For nodemon restarts
+
+process.once("SIGUSR2", () => {
+  gracefulShutdown("nodemon restart", () => {
+    process.kill(process.pid, "SIGUSR2");
+  });
+});
+
+// For app termination
+
+process.on("SIGINT", () => {
+  gracefulShutdown("app termination", () => {
+    process.exit(0);
+  });
 });
