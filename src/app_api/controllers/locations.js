9390a90a5e54c3bdbe63d0de4c3ec49a8c8d0cd9
Bruno <brunohgrau99@gmail.com>
2024-02-27 00:49:39 -0300

    New Commit

--- src/app_api/controllers/locations.js
+++ src/app_api/controllers/locations.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Loc = mongoose.model("Location");
 
+// Not working
 const locationsListByDistance = async (req, res) => {
   const lng = parseFloat(req.query.lng);
   const lat = parseFloat(req.query.lat);
@@ -80,23 +81,6 @@ const locationsReadOne = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
-/* 
-
-const locationsReadOne = (req, res) => {
-  Loc.findById(req.params.locationid).exec((err, location) => {
-    if (!location) {
-      return res.status(404).json({ message: "location not found" });
-    } else if (err) {
-      return res.status(404).json(err);
-    }
-    res.status(200).json(location);
-  });
-  res.status(200).json({ status: "success" });
-};
-
-
-*/
 
 const locationsUpdateOne = (req, res) => {
   res.status(200).json({ status: "success" });
--- src/app_api/controllers/reviews.js
+++ src/app_api/controllers/reviews.js
@@ -1,4 +1,44 @@
-const reviewsCreate = (req, res) => {};
+const mongoose = require("mongoose");
+const Loc = mongoose.model("Location");
+
+const reviewsCreate = async (req, res) => {
+  try {
+    const locationId = req.params.locationid;
+
+    if (locationId) {
+      const location = await Loc.findById(locationId).select("reviews");
+
+      await doAddReview(req, res, location);
+    } else {
+      res.status(404).json({ message: "Location not found" });
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+const doAddReview = async (req, res, location) => {
+  try {
+    if (!location) {
+      res.status(404).json({ message: "Location not found" });
+      return; // Exit the function early if location not found
+    }
+
+    const { author, rating, reviewText } = req.body;
+
+    location.reviews.push({ author, rating, reviewText });
+
+    await location.save();
+
+    await updateAverageRating(location._id);
+
+    const thisReview = location.reviews.slice(-1).pop();
+
+    res.status(201).json(thisReview);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
 const reviewsReadOne = (req, res) => {};
 const reviewsUpdateOne = (req, res) => {};
 const reviewsDeleteOne = (req, res) => {};
